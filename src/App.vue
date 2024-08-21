ef963cce0ae74eb19d435ea5062d17fa406dd430
17521187214 <fanfangloveyou@gmail.com>
2024-08-22 07:31:56 +0800

    1st

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+</template>
+
+<style scoped>
+h1 {
+  color: #fabb07;
+}
+</style>
